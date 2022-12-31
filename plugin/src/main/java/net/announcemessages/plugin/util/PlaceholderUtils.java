0ce71101dc72d8929c0b46380961ca390710779c
InitSync <benjaalejandro206@gmail.com>
2022-12-31 20:29:30 -0300

    feat(PlaceholderUtils): add utility class for manage the PlaceholderAPI integration and compatibility

---
+++ plugin/src/main/java/net/announcemessages/plugin/util/PlaceholderUtils.java
@@ -0,0 +1,21 @@
+package net.announcemessages.plugin.util;
+
+import com.google.common.base.Preconditions;
+import me.clip.placeholderapi.PlaceholderAPI;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+public class PlaceholderUtils {
+	private static final boolean PLACEHOLDERS = Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null &&
+		 Bukkit.getPluginManager().isPluginEnabled("PlaceholderAPI");
+	
+	private PlaceholderUtils() {}
+	
+	public static String parse(Player player, String text) {
+		Preconditions.checkNotNull(player, "The player is null.");
+		
+		if (!PLACEHOLDERS) return text;
+		
+		return PlaceholderAPI.setPlaceholders(player, text);
+	}
+}
