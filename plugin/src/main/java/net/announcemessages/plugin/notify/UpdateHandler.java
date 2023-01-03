a783d8d705681b4525cdfa2a1e2e62ce62708198
InitSync <benjaalejandro206@gmail.com>
2023-01-03 20:10:58 -0300

    refactor(UpdateHandler): remove `configurationHandler` field and use static `AnnounceMessage#getPlugin` for get the instance of `BukkitConfigurationHandler`

--- plugin/src/main/java/net/announcemessages/plugin/notify/UpdateHandler.java
+++ plugin/src/main/java/net/announcemessages/plugin/notify/UpdateHandler.java
@@ -1,20 +1,17 @@
 package net.announcemessages.plugin.notify;
 
+import net.announcemessages.plugin.AnnounceMessages;
 import net.announcemessages.plugin.util.LogUtils;
-import net.xconfig.bukkit.config.BukkitConfigurationHandler;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.Objects;
 import java.util.Scanner;
 import java.util.function.Consumer;
 
 public class UpdateHandler {
-	private final BukkitConfigurationHandler configurationHandler;
 	private final int resourceNumber;
 	
-	public UpdateHandler(BukkitConfigurationHandler configurationHandler, int resourceNumber) {
-		this.configurationHandler = Objects.requireNonNull(configurationHandler, "The BukkitConfigurationHandler object is null.");
+	public UpdateHandler(int resourceNumber) {
 		this.resourceNumber = resourceNumber;
 	}
 	
@@ -24,7 +21,12 @@ public class UpdateHandler {
 			if (scanner.hasNext()) consumer.accept(scanner.next());
 		} catch (IOException exception) {
 			LogUtils.error("Unable to check for updates available.");
-			if (configurationHandler.condition("", "config.yml", "config.debug-mode")) exception.printStackTrace();
+		
+			if (AnnounceMessages.getPlugin()
+				 .getConfigurationHandler()
+				 .condition("", "config.yml", "config.debug-mode")) {
+				exception.printStackTrace();
+			}
 		}
 	}
 }
