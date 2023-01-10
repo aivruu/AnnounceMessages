3b52a4f0d9df16f0d66b38ecf48fbd119ecb9bf9
InitSync <benjaalejandro206@gmail.com>
2023-01-10 13:27:03 -0300

    chore(Utils): remove checks and implement `sendTabList()` method

--- plugin/src/main/java/net/announcemessages/plugin/util/Utils.java
+++ plugin/src/main/java/net/announcemessages/plugin/util/Utils.java
@@ -1,5 +1,6 @@
 package net.announcemessages.plugin.util;
 
+import com.google.common.base.Preconditions;
 import net.announcemessages.plugin.AnnounceMessages;
 import net.announcemessages.plugin.xseries.XSound;
 import net.xconfig.bukkit.utils.TextUtils;
@@ -7,38 +8,46 @@ import net.xtitle.api.TitleManager;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-import static com.google.common.base.Preconditions.checkArgument;
-
 public class Utils {
 	private static final TitleManager MANAGER = AnnounceMessages.getPlugin().getTitleManager();
 	
 	private Utils() {}
 	
 	public static void sendTitle(Player player, String title, String subtitle, int fadeIn, int stay, int fadeOut) {
-		checkNotNull(player, "The player is null.");
-		checkArgument(!title.isEmpty(), "The title message is empty.");
-		checkArgument(!subtitle.isEmpty(), "The subtitle message is empty.");
-		
-		MANAGER.sendTitle(player, fadeIn, stay, fadeOut, TextUtils.colorize(title), TextUtils.colorize(subtitle));
+		MANAGER.sendTitle(
+			 player,
+			 fadeIn,
+			 stay,
+			 fadeOut,
+			 TextUtils.colorize(PlaceholderUtils.parse(player, title)),
+			 TextUtils.colorize(PlaceholderUtils.parse(player, subtitle))
+		);
 	}
 	
 	public static void clearTitle(Player player) {
-		checkNotNull(player, "The player is null.");
-		
 		MANAGER.clearTitle(player);
 	}
 	
+	public static void sendTabList(Player player, String footer, String header) {
+		MANAGER.sendTabList(
+			 player,
+			 TextUtils.colorize(PlaceholderUtils.parse(player, footer)),
+			 TextUtils.colorize(PlaceholderUtils.parse(player, header))
+		);
+	}
+	
 	public static void sendActionBar(JavaPlugin plugin, Player player, String message, long duration) {
-		checkNotNull(player, "The player is null.");
-		checkArgument(!message.isEmpty(), "The message is empty.");
-		
-		MANAGER.sendActionBar(plugin, player, message, duration);
+		MANAGER.sendActionBar(
+			 plugin,
+			 player,
+			 TextUtils.colorize(PlaceholderUtils.parse(player, message)),
+			 duration
+		);
 	}
 	
 	public static void playSound(Player player, String sound, int volume, int pitch) {
-		checkNotNull(player, "The player is null.");
-		checkArgument(!sound.isEmpty(), "The sound type is empty.");
+		Preconditions.checkNotNull(player, "The player is null.");
+		Preconditions.checkArgument(!sound.isEmpty(), "The sound type is empty.");
 		
 		player.playSound(player.getLocation(), XSound.matchXSound(sound.toUpperCase()).get().parseSound(), volume, pitch);
 	}
